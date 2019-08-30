c9c6fe1d4eab5e6d94dd29198f9e22151fc7083f
unknown <j.arturo.bautista123@gmail.com>
2019-08-30 10:49:58 -0500

    1°Se realizo el ejemplo calculadora para entender los metodos de asignacion en este caso a una suma.

--- ejemplo1/app.js
+++ ejemplo1/app.js
@@ -1,6 +1,30 @@
+//esta linea crea una variable lacual se ebcarga de obtener por medio del objeto documen en el elemento button con el id "btnCalcular"
+
 let btncalcular = document.getElementById('btncalcular');
+
+//por medio del método "addEventListener" se le asigno el evento click al evento "button" y al momento de ejecutar dicho evento se ejecutara la funcion suma.
 btncalcular.addEventListener('click', suma);
 
+
+//funcion: bloque de codigo que se ejecuta hasta cuando se manda a llamar o se invoca, mientras no sea invocada no realizara ninguna acccion en el codigo.
 function suma() {
-    console.log('x')
+
+    //se obtendran los valores de los inputs por medio del metodo ".value" 
+    let Numero1 = document.getElementById('numero1').value;
+    let Numero2 = document.getElementById('numero2').value;
+
+    //se reasigna el valor de la variable numero2 convirtiendolo de String a Numero.
+    Numero1 = Number(Numero1);
+    Numero2 = Number(Numero2);
+
+    //se declara la variable "resultado" y se inicializa con valor '0'
+    let resultado = 0;
+
+    //despues de obtener los datos de los elementos input se realiza el proceso de suma y se le asigna dicho valor a la variable "resultado"
+    resultado = Numero1 + Numero2;
+
+    //se asigno a la variable "imprimirResultado" el elemento con el id "resultado" 
+    let imprimirResultado = document.getElementById('resultado');
+    //por medio del metodo "innerHTML" se le asigna el valor de la variable resultado para que pueda imprimir en el elemento
+    imprimirResultado.innerHTML = resultado;
 }
